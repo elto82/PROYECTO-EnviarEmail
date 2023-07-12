bc78f7655e0ef89fadd1239903c369d7db0a346f
Argiro <elto.82@gmai.com>
2023-07-11 23:33:25 -0500

    ejemplo de formulario con js

---
+++ js/app.js
@@ -0,0 +1,133 @@
+//variables
+const email = document.querySelector("#email");
+const asunto = document.querySelector("#asunto");
+const mensaje = document.querySelector("#mensaje");
+const btnEnviar = document.querySelector("#enviar");
+const formulario = document.querySelector("#enviar-mail");
+const resetBtn = document.querySelector("#resetBtn");
+const er =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+eventListeners();
+
+function eventListeners() {
+  document.addEventListener("DOMContentLoaded", iniciarApp);
+
+  //campos del formulario
+  email.addEventListener("blur", validarFormulario);
+  asunto.addEventListener("blur", validarFormulario);
+  mensaje.addEventListener("blur", validarFormulario);
+
+  //enviar email
+  formulario.addEventListener("submit", enviarEmail);
+  //reiniciar formulario
+  resetBtn.addEventListener("click", resetearFormulario);
+}
+
+//funciones
+
+function iniciarApp() {
+  btnEnviar.disabled = true;
+  btnEnviar.classList.add("cursor-not-allowed", "opacity-50");
+}
+
+function validarFormulario(e) {
+  if (e.target.value.length > 0) {
+    const error = document.querySelector("p.error");
+    if (error) {
+      error.remove();
+    }
+    e.target.classList.remove("border", "border-red-500");
+    e.target.classList.add("border", "border-green-500");
+  } else {
+    e.target.classList.remove("border", "border-green-500");
+    e.target.classList.add("border", "border-red-500");
+    mostrarError("Todos los campos son obligatorios");
+  }
+
+  if (e.target.type === "email") {
+    if (er.test(e.target.value)) {
+      const error = document.querySelector("p.error");
+      if (error) {
+        error.remove();
+      }
+
+      e.target.classList.remove("border", "border-red-500");
+      e.target.classList.add("border", "border-green-500");
+    } else {
+      e.target.classList.remove("border", "border-green-500");
+      e.target.classList.add("border", "border-red-500");
+      mostrarError("Email no valido");
+    }
+  }
+
+  if (er.test(email.value) && asunto.value !== "" && mensaje.value !== "") {
+    btnEnviar.disabled = false;
+    btnEnviar.classList.remove("cursor-not-allowed", "opacity-50");
+  } else {
+    mostrarError("Hay campos vacios");
+  }
+}
+
+function mostrarError(mensaje) {
+  const mensajeError = document.createElement("p");
+  mensajeError.textContent = mensaje;
+  mensajeError.classList.add(
+    "border",
+    "border-red-500",
+    "background-red-100",
+    "text-red-500",
+    "p-3",
+    "mt-5",
+    "text-center",
+    "error"
+  );
+  const errores = document.querySelectorAll(".error");
+  if (errores.length === 0) {
+    formulario.appendChild(mensajeError);
+  }
+}
+
+function enviarEmail(e) {
+  e.preventDefault();
+  //mostrar spinner
+  const spinner = document.querySelector("#spinner");
+  spinner.style.display = "flex";
+
+  //despues de 3 segundos ocultar spinner
+  setTimeout(() => {
+    spinner.style.display = "none";
+
+    //mensaje de envio exitoso
+    const parrafo = document.createElement("p");
+    parrafo.textContent = "El mensaje se envio correctamente";
+    parrafo.classList.add(
+      "text-center",
+      "my-10",
+      "p-2",
+      "bg-green-500",
+      "text-white",
+      "fond-bold",
+      "uppercase"
+    );
+    //insertar el parrafo antes del spinner
+    formulario.insertBefore(parrafo, spinner);
+    //limpiar
+    resetearFormulario(e);
+    //borrar el mensaje de envio exitoso
+    setTimeout(() => {
+      parrafo.remove();
+      iniciarApp();
+    }, 2000);
+  }, 3000);
+}
+
+function resetearFormulario(e) {
+  e.preventDefault();
+  formulario.reset();
+
+  const parrafo = document.querySelector("p.text-center");
+  if (parrafo) {
+    parrafo.remove();
+  }
+}
